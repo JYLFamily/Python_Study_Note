02e7a4b3470533c296129b9a36d7a2b7b16b8184
JYLFamily <1579502635@qq.com>
2017-10-13 22:48:48 +0800

    日常同步

--- Other/tools/read_line.py
+++ Other/tools/read_line.py
@@ -3,19 +3,32 @@ import os
 
 
 def read_txt():
-    # 返回一个文件对象
-    f = open("C:\\Users\\Dell\\Desktop\\nohup.out")
-    # 调用文件的 readline()方法
-    line = f.readline()
-    while line:
-        # os.linesep 返回适合系统的换行符号 , 遇到空行跳过
-        if line == os.linesep:
-            continue
-        if line.split(sep=" ")[0] == "cluster":
-            print(line.split(sep=" ")[4], end="")
-        line = f.readline()
+    # try:
+    #     # 返回一个文件对象
+    #     f = open("C:\\Users\\Dell\\Desktop\\nohup.out", "r")
+    #     # 调用文件的 readline()方法
+    #     line = f.readline()
+    #     while line:
+    #         # os.linesep 返回适合系统的换行符号 , 遇到空行跳过
+    #         if line == os.linesep:
+    #             continue
+    #         if line.split(sep=" ")[0] == "cluster":
+    #             # print() 函数不换行
+    #             print(line.split(sep=" ")[4], end="")
+    #         line = f.readline()
+    # finally:
+    #     f.close()
 
-    f.close()
+    with open("C:\\Users\\Dell\\Desktop\\nohup.out", "r") as f:
+        line = f.readline()
+        while line:
+            # os.linesep 返回适合系统的换行符号 , 遇到空行跳过
+            if line == os.linesep:
+                continue
+            if line.split(sep=" ")[0] == "cluster":
+                # print() 函数不换行
+                print(line.split(sep=" ")[4], end="")
+            line = f.readline()
 
 if __name__ == "__main__":
     read_txt()
