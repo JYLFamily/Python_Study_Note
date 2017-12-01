2fe90cf10d4570c315ff3701570aec4557febc62
JYLFamily <1579502635@qq.com>
2017-12-01 10:32:02 +0800

    添加 joblib 分类相关内容

---
+++ Sklearn/model_persistence/JoblibTry.py
@@ -0,0 +1,22 @@
+# coding:utf-8
+
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.externals import joblib
+
+
+class JoblibTry(object):
+
+    def __init__(self):
+        self.__model = DecisionTreeClassifier()
+
+    def model_dump(self):
+        joblib.dump(self.__model, "C:\\Users\\Dell\\Desktop\\drt.pkl.z", True)
+
+    def model_load(self):
+        self.__model = joblib.load("C:\\Users\\Dell\\Desktop\\drt.pkl.z")
+
+
+if __name__ == "__main__":
+    jbt = JoblibTry()
+    jbt.model_dump()
+    jbt.model_load()
