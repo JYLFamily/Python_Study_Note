775f3b5c76217c019c5e3d412fbc1411391e4690
JYLFamily <1579502635@qq.com>
2017-12-19 13:46:05 +0800

    mxnet/Gluon 实现线性回归

--- Mxnet/Linear_Regression/Gluon.py
+++ Mxnet/Linear_Regression/Gluon.py
@@ -1,62 +1,88 @@
 # coding:utf-8
 
+# coding:utf-8
+
+
 from mxnet import ndarray as nd
 from mxnet import autograd
 from mxnet import gluon
 
 
 class Gluon(object):
-
     def __init__(self):
+        # 构造测试数据
+        self.__num_inputs = None
+        self.__num_examples = None
+        self.__true_w = None
+        self.__true_b = None
+        self.__X = None
+        self.__y = None
+
+        # function set
+        self.__net = None
+
+        # goodness of function loss function
+        self.__loss_function = None
+
+        # goodness of function optimizer data
+        self.__batch_size = None
+        self.__data_iter = None
+
+        # goodness of function optimizer function
+        self.__trainer = None
+        self.__learning_rate = None
+
+        # pick the best function 模型训练
+        self.__epochs = None
+        self.__batch_X = None
+        self.__batch_y = None
+        self.__batch_y_hat = None
+
+    def data_prepare(self):
         self.__num_inputs = 2
         self.__num_examples = 1000
-
         self.__true_w = nd.array([2, -3.4]).reshape((2, 1))
         self.__true_b = nd.array([4.2]).reshape((1, 1))
-
         self.__X = nd.random_normal(shape=(self.__num_examples, self.__num_inputs))
         self.__y = nd.dot(self.__X, self.__true_w) + self.__true_b
-        self.__y += 0.01 * nd.random_normal(shape=self.__y.shape)
+        self.__y += 0.001 * nd.random_normal(shape=self.__y.shape)
 
-        self.__batch_size = 10
-        self.__data_iter = None
-        self.__net = None
-        self.__loss_function = None
-        self.__trainer = None
-
-    def set_data_iter(self):
-        self.__data_iter = (gluon.data.DataLoader(
-            gluon.data.ArrayDataset(self.__X, self.__y), self.__batch_size, shuffle=True))
-
-    def set_model(self):
+    def function_set(self):
         self.__net = gluon.nn.Sequential()
         self.__net.add(gluon.nn.Dense(1))
         self.__net.initialize()
 
-    def set_loss_function(self):
+    def goodness_of_function_loss_function(self):
         self.__loss_function = gluon.loss.L2Loss()
 
-    def train_model(self):
-        self.__trainer = gluon.Trainer(
-            self.__net.collect_params(), "sgd", {"learning_rate": 0.1})
+    def goodness_of_function_optimizer_data(self):
+        self.__batch_size = 100
+        self.__data_iter = (gluon.data.DataLoader(
+            gluon.data.ArrayDataset(self.__X, self.__y), self.__batch_size, shuffle=True))
 
-        epochs = 5
-        batch_size = 10
-        for e in range(epochs):
+    def goodness_of_function_optimizer_function(self):
+        self.__learning_rate = 1
+        self.__trainer = gluon.Trainer(self.__net.collect_params(), "sgd", {"learning_rate": self.__learning_rate})
+
+    def train_model(self):
+        self.__epochs = 5
+        for e in range(self.__epochs):
             total_loss = 0
-            for data, label in self.__data_iter:
+            for self.__batch_X, self.__batch_y in self.__data_iter:
                 with autograd.record():
-                    output = self.__net(data)
-                    loss = self.__loss_function(output, label)
+                    self.__batch_y_hat = self.__net(self.__batch_X)
+                    loss = self.__loss_function(self.__batch_y_hat, self.__batch_y)
                 loss.backward()
-                self.__trainer.step(batch_size)
+                self.__trainer.step(self.__batch_size)
                 total_loss += nd.sum(loss).asscalar()
-            print("Epoch %d, average loss: %f" % (e, total_loss / self.__num_examples))
+            print("Epoch %d, average loss: %f" % (e, total_loss))
 
 
 if __name__ == "__main__":
-    g = Gluon()
-    g.set_data_iter()
-    g.set_model()
-    g.set_loss_function()
-    g.train_model()
+    s = Gluon()
+    s.data_prepare()
+    s.function_set()
+    s.goodness_of_function_loss_function()
+    s.goodness_of_function_optimizer_data()
+    s.goodness_of_function_optimizer_function()
+    s.train_model()
--- Mxnet/Linear_Regression/Scratch.py
+++ Mxnet/Linear_Regression/Scratch.py
@@ -1,76 +1,90 @@
 # coding:utf-8
 
 import random
-import numpy as np
 from mxnet import ndarray as nd
 from mxnet import autograd
-np.random.seed(9)
 
 
 class Scratch(object):
 
     def __init__(self):
-        # X shape (1000, 2)
-        self.__num_inputs = 2
-        self.__num_examples = 1000
+        # 构造测试数据
+        self.__num_inputs = None
+        self.__num_examples = None
+        self.__true_w = None
+        self.__true_b = None
+        self.__X = None
+        self.__y = None
 
-        self.__true_w = np.array([2, -3.4]).reshape((2, 1))
-        self.__true_b = 4.2
+        # function set 待估计参数
+        self.__w = None
+        self.__b = None
+        self.__params = None
 
-        self.__X = np.random.normal(size=(self.__num_examples, self.__num_inputs))
-        self.__y = np.dot(self.__X, self.__true_w) + self.__true_b
-        self.__y = self.__y + 0.01 * np.random.normal(size=(self.__num_examples, 1))
-        # np.array -> nd.array
-        self.__X = nd.array(self.__X)
-        self.__y = nd.array(self.__y)
+        # goodness of function loss function
 
-        self.__w = nd.array(np.random.normal(size=(2, 1)))
-        self.__b = nd.array(np.random.normal(size=(1, 1)))
-        self.__params = [self.__w, self.__b]
+        # goodness of function optimizer data
+        self.__batch_size = None
 
-        self.__batch_size = 500
+        # goodness of function optimizer function
+        self.__learning_rate = None
+
+        # pick the best function 模型训练
+        self.__epochs = None
         self.__batch_X = None
         self.__batch_y = None
         self.__batch_y_hat = None
 
-        self.__epochs = 5
-        self.__learning_rate = 0.001
+    def data_prepare(self):
+        self.__num_inputs = 2
+        self.__num_examples = 1000
+        self.__true_w = nd.array([2, -3.4]).reshape((2, 1))
+        self.__true_b = nd.array([4.2]).reshape((1, 1))
+        self.__X = nd.random_normal(shape=(self.__num_examples, self.__num_inputs))
+        self.__y = nd.dot(self.__X, self.__true_w) + self.__true_b
+        self.__y += 0.001 * nd.random_normal(shape=self.__y.shape)
 
-    def attach_gradient(self):
-        for param in self.__params:
-            param.attach_grad()
+        self.__w = nd.random_normal(shape=(2, 1))
+        self.__b = nd.random_normal(shape=(1, 1))
+        self.__params = [self.__w, self.__b]
 
-    def data_iter(self):
+    def function_set(self):
+        return nd.dot(self.__batch_X, self.__w) + self.__b
+
+    def goodness_of_function_loss_function(self):
+        return (self.__batch_y - self.__batch_y_hat) ** 2
+
+    def goodness_of_function_optimizer_data(self):
+        self.__batch_size = 100
         idx = list(range(self.__num_examples))
         random.shuffle(idx)
         for i in range(0, self.__num_examples, self.__batch_size):
             j = nd.array(idx[i:min(i + self.__batch_size, self.__num_examples)])
             yield nd.take(self.__X, j).reshape((-1, 2)), nd.take(self.__y, j).reshape((-1, 1))
 
-    def net(self):
-        return nd.dot(self.__batch_X, self.__w) + self.__b
-
-    def square_loss(self):
-        return (self.__batch_y_hat - self.__batch_y) ** 2
-
-    def stochastic_gradient_descent(self):
+    def goodness_of_function_optimizer_function(self):
+        self.__learning_rate = 0.001
         for param in self.__params:
             param[:] = param - self.__learning_rate * param.grad
 
     def train_model(self):
+        for param in self.__params:
+            param.attach_grad()
+
+        self.__epochs = 5
         for e in range(self.__epochs):
             total_loss = 0
-            for self.__batch_X, self.__batch_y in self.data_iter():
+            for self.__batch_X, self.__batch_y in self.goodness_of_function_optimizer_data():
                 with autograd.record():
-                    self.__batch_y_hat = self.net()
-                    loss = self.square_loss()
+                    self.__batch_y_hat = self.function_set()
+                    loss = self.goodness_of_function_loss_function()
                 loss.backward()
-                self.stochastic_gradient_descent()
+                self.goodness_of_function_optimizer_function()
                 total_loss += nd.sum(loss).asscalar()
             print("Epoch %d, average loss: %f" % (e, total_loss))
 
 
 if __name__ == "__main__":
     s = Scratch()
-    s.attach_gradient()
+    s.data_prepare()
     s.train_model()
