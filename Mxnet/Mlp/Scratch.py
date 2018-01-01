ccbf84ef95ea75db5e3f9a72754f8948f2b7e757
JYLFamily <1579502635@qq.com>
2018-01-01 15:59:07 +0800

    MLP 相关内容

--- Mxnet/Mlp/Scratch.py
+++ Mxnet/Mlp/Scratch.py
@@ -1,27 +1,28 @@
 # coding:utf-8
 
-from mxnet import gluon
 from mxnet import ndarray as nd
 from mxnet import autograd
+from mxnet import gluon
 
 
 class Scratch(object):
 
-    def __init__(self, *, batch_size, learning_rate, epochs):
+    def __init__(self, *, num_hidden, batch_size, learning_rate, epochs):
         # data prepare
-        # 原始数据包含 X, y
         self.__num_output = 10
         self.__num_input = 784
         self.__train = None
         self.__test = None
 
-        self.__w = None
-        self.__b = None
+        # parameters
+        self.__num_hidden = num_hidden
+        self.__w1 = None
+        self.__b1 = None
+        self.__w2 = None
+        self.__b2 = None
         self.__params = None
 
-        # function set
-
-        # goodness of function loss function
+        # function set & goodness of function loss function
 
         # goodness of function optimizer data
         self.__batch_size = batch_size
@@ -35,7 +36,6 @@ class Scratch(object):
         self.__epochs = epochs
         self.__batch_X = None
         self.__batch_y = None
-        self.__batch_y_hat = None
 
     def data_prepare(self):
         def transform(data, label):
@@ -43,21 +43,21 @@ class Scratch(object):
         self.__train = gluon.data.vision.FashionMNIST(train=True, transform=transform)
         self.__test = gluon.data.vision.FashionMNIST(train=False, transform=transform)
 
-        # 10 分类问题相当于有 10 个 Logistics Regression self.__num_output
-        # 每个 Logistics Regression 接收 784 个特征 self.__num_input
-        self.__w = nd.random_normal(shape=(self.__num_input, self.__num_output))
-        self.__b = nd.random_normal(shape=(1, self.__num_output))
-        self.__params = [self.__w, self.__b]
+        self.__w1 = nd.random_normal(shape=(self.__num_input, self.__num_hidden))
+        self.__b1 = nd.random_normal(shape=(1, self.__num_hidden))
+        self.__w2 = nd.random_normal(shape=(self.__num_hidden, self.__num_output))
+        self.__b2 = nd.random_normal(shape=(1, self.__num_output))
+        self.__params = [self.__w1, self.__b1, self.__w2, self.__b2]
 
-    def function_set(self):
-        z = nd.dot(self.__batch_X.reshape((-1, self.__num_input)), self.__w) + self.__b
-        exp_z = nd.exp(z)
-        partition = exp_z.sum(axis=1, keepdims=True)
+    def function_set_loss_function(self):
+        def relu(batch_X):
+            return nd.maximum(batch_X, 0)
+        self.__batch_X = nd.dot(self.__batch_X.reshape((-1, self.__num_input)), self.__w1) + self.__b1
+        h1_X = relu(self.__batch_X)
+        self.__batch_X = nd.dot(h1_X, self.__w2) + self.__b2
+        loss = gluon.loss.SoftmaxCrossEntropyLoss(self.__batch_X, self.__batch_y)
 
-        return exp_z / partition
-
-    def goodness_of_function_loss_function(self):
-        return - nd.log(nd.pick(self.__batch_y_hat, self.__batch_y))
+        return loss
 
     def goodness_of_function_optimizer_data(self):
         self.__train_data_iter = gluon.data.DataLoader(
@@ -69,6 +69,7 @@ class Scratch(object):
         for param in self.__params:
             param[:] = param - self.__learning_rate / self.__batch_size * param.grad
 
+
     def train_model(self):
         for param in self.__params:
             param.attach_grad()
@@ -77,8 +78,7 @@ class Scratch(object):
             total_loss = 0
             for self.__batch_X, self.__batch_y in self.__train_data_iter:
                 with autograd.record():
-                    self.__batch_y_hat = self.function_set()
-                    loss = self.goodness_of_function_loss_function()
+                    loss = self.function_set_loss_function()
                 loss.backward()
                 self.goodness_of_function_optimizer_function()
                 total_loss += nd.mean(loss).asscalar()
@@ -87,7 +87,7 @@ class Scratch(object):
 
 
 if __name__ == "__main__":
-    s = Scratch(batch_size=256, learning_rate=0.1, epochs=5)
+    s = Scratch(num_hidden=256, batch_size=256, learning_rate=0.1, epochs=5)
     s.data_prepare()
     s.goodness_of_function_optimizer_data()
     s.train_model()
