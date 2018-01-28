434587e15a8ef7596bcf09591ac4865228baa989
JYLFamily <1579502635@qq.com>
2018-01-28 15:05:22 +0800

    日常更新

--- Keras/morvan/RnnLstmDemo.py
+++ Keras/morvan/RnnLstmDemo.py
@@ -4,6 +4,7 @@ import random
 import numpy as np
 from keras.models import Sequential
 from keras.layers import LSTM
+from keras.layers import TimeDistributed
 from keras.layers import Dense
 np.random.seed(9)
 
@@ -27,8 +28,17 @@ class RnnLstmDemo(object):
             units=self.__units,
             input_length=self.__input_length,
             input_dim=self.__input_dim,
-            return_sequences=False
+            return_sequences=True,
+            # stateful=True 代表一个 batch 中下标为 i 的样本的最终状态将会用作下一个 batch 同样下标的样本的初始状态 , stateful=True
+            # 需要指定 batch_size 这里没有指定 , 所以将 stateful=True 注释掉
+            # stateful=True
         ))
+
+        # self.__net.add(TimeDistributed(Dense(
+        #     units=self.__output_dim,
+        #     activation="softmax"
+        # )))
+
         self.__net.add(Dense(
             units=self.__output_dim,
             activation="softmax"
